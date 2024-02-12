05b12891b101bd226915cc900202f31159d78c9a
AlizerUncaged <saylent1@gmail.com>
2024-02-13 00:50:54 +0800

    🗃️ add voice migrations

---
+++ Waifu/Migrations/20240212142529_ElevellabsToken.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Waifu.Migrations
+{
+    /// <inheritdoc />
+    public partial class ElevellabsToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ElevenlabsApiKey",
+                table: "settings",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ElevenlabsApiKey",
+                table: "settings");
+        }
+    }
+}
---
+++ Waifu/Migrations/20240212163722_AudioPlayer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Waifu.Migrations
+{
+    /// <inheritdoc />
+    public partial class AudioPlayer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AudioPlayerDeviceId",
+                table: "settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EnableElevenlabs",
+                table: "settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AudioPlayerDeviceId",
+                table: "settings");
+
+            migrationBuilder.DropColumn(
+                name: "EnableElevenlabs",
+                table: "settings");
+        }
+    }
+}
